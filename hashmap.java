d7210c7084b24a33c97ec79d6609ad9f2f8b26d8
pranavvdesai <pranavdesai.psd@gmail.com>
2022-03-30 08:34:35 +0530

    searchword

--- hashmap.java
+++ hashmap.java
@@ -8,6 +8,21 @@ public class hashmap {
         hm.put("php", "c++");
         System.out.println(hm);
         hm.get("c");
+        Map<String, String> hm2 = new HashMap<String, String>();
+        FileInputStream fis = new FileInputStream("/home/saurabh/Desktop/hashmap.java");
+        Scanner sc = new Scanner(fis);
+        while (sc.hasNext()) {
+            String word = sc.nextLine()
+            String split = word.split(" ");
+            for (int i = 0; i < split.length; i++) {
+                if (hm2.containsKey(split[i])) {
+                    hm2.put(split[i], hm2.get(split[i]) + 1);
+                } else {
+                    hm2.put(split[i], 1);
+                }
+            }
+
+        }
 
     }
 }
